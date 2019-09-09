908d0a5a81f0227828851d44be604b0ea5498968
정선 <jspark7373@naver.com>
2019-09-10 04:52:57 +0900

    Sdf

--- src/components/learn/codeBlock.js
+++ src/components/learn/codeBlock.js
@@ -14,6 +14,7 @@ class CodeBlock extends Component {
     }
     onChangeState(code, workspace) {
         this.props.fetchCode(code, workspace);
+        console.log(workspace);
     }
     render() {
         return(
