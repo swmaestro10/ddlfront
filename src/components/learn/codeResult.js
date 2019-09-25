f6fd9af45ade440ebe404e0dcdfd4e7ff282543a
정선 <jspark7373@naver.com>
2019-09-25 18:58:42 +0900

    Sdf

--- src/components/learn/codeResult.js
+++ src/components/learn/codeResult.js
@@ -3,20 +3,20 @@ import { connect } from 'react-redux';
 
 
 class CodeResult extends Component {
-    renderCode() {
-        let results = this.props.result;
-        return results.map((result, index) => {
-            return (
-                <li key={index} className="code"> {result}</li>
-            )
-        });
-    }
+    // renderCode() {
+    //     let results = this.props.result;
+    //     return results.map((result, index) => {
+    //         return (
+    //             <li key={index} className="code"> {result}</li>
+    //         )
+    //     });
+    // }
     render() {
         return(
             <div className={this.props.index === 2 ? "exist" : "non-exist"}>
                 <div className="code-text">
                     <ul>
-                        {this.renderCode()}
+                        {/* {this.renderCode()} */}
                     </ul>
                 </div>
             </div>
--- src/reducers/resultReducer.js
+++ src/reducers/resultReducer.js
@@ -3,6 +3,7 @@ import { SUBMIT_CODE } from '../actions/codeFunction';
 export default function(state=[], action) {
     switch(action.type) {
         case SUBMIT_CODE:
+            console.log(state);
             return [action.payload, ...state];
         default:
             return state;
