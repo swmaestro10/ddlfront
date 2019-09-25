38802a5898935e01b5730d1581f74dbf186463de
정선 <jspark7373@naver.com>
2019-09-25 18:35:33 +0900

    Sdf

--- src/reducers/resultReducer.js
+++ src/reducers/resultReducer.js
@@ -3,6 +3,7 @@ import { SUBMIT_CODE } from '../actions/codeFunction';
 export default function(state=[], action) {
     switch(action.type) {
         case SUBMIT_CODE:
+            console.log(action.payload);
             return [action.payload, ...state];
         default:
             return state;
