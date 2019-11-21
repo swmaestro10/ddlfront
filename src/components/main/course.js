bc49d4daa795d421e8c760f59883f29cd8b955e4
sunimooni <jspark7373@naver.com>
2019-11-21 22:43:54 +0900

    sdf

--- src/components/main/course.js
+++ src/components/main/course.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-import howtouse from "../img/main_howtouse.png";
-
 class Course extends Component {
   render() {
     const { info } = this.props;
@@ -10,19 +8,18 @@ class Course extends Component {
     return (
       <div className="course">
         <div className="box-knowledge">
-          <p> [기본 튜토리얼] </p>
-          {/* <p>{info.knowledge}</p> */}
-          <img
-            className="tooltip"
-            src={howtouse}
-            alt="howtouse"
-            height="30"
-            width="30"
-          />
+          {/* <p> [기본 튜토리얼] </p> */}
+          <p>{info.knowledge}</p>
+          <div className="tooltip">
+            [ TIP ]
+            <div>
+              <p>뚝딱러닝 기본 튜토리얼</p> <p>블록 사용법 제공</p>
+            </div>
+          </div>
         </div>
         <div className="box-title">
-          <p> MNIST 체험 </p>
-          {/* <p>{info.name}</p> */}
+          {/* <p> MNIST 체험 </p> */}
+          <p>{info.name}</p>
         </div>
         <div className="box-summary">{/* <p>{info.explain}</p> */}</div>
         <div className="inclass">
